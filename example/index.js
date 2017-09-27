e7bea51a56135455421e778b33b6036266e6ded0
方永乐 <fangyongle@innobuddy.com>
2017-09-27 11:05:08 +0800

    add calc string

--- example/index.js
+++ example/index.js
@@ -1,8 +1,18 @@
-var strDOMLength = require('../src/index');
+var EasyFont = require('../src/index');
 
-strDOMLength('Skylor.min')
-.then(function(data) {
-  if (data) {
-    console.log(data);
-  }
-});
+new EasyFont().pipe('Skylor.min is very handsome!')
+    .operation('measure')
+    .option({ fontSize: 14, fontFamily: "'PingFang SC', sans-serif" })
+    .value()
+    .then(function (result) {
+      console.log(result);
+    });
+
+
+new EasyFont().pipe('Skylor.min is very handsome!')
+  .operation('truncate')
+  .option({ width: 20, row: 2, fontSize: 14, fontFamily: "'PingFang SC', sans-serif" })
+  .value()
+  .then(function (result) {
+    console.log(result);
+  });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,138 @@
 'use strict';
 
 var Font = require('./lib/font.js');
-var strDOMLength = function (str, option) {
-  return new Promise(function(resolve) {
+
+// 母体
+function EasyFont() {
+  this.$string = '';  // 初始值
+  this.$option = {}; // 配置信息
+  this.$result = {}; // 输出值
+  this.$operationName = ''; // 操作
+  this._settedOption = false;
+}
+
+/**
+ * 入口函数
+ * @param str 输入的字符串
+ */
+EasyFont.prototype.pipe = function(str) {
+  this.$string = str;
+  return this;
+}
+
+/**
+ * 配置参数
+ * @param option {object} 操作参数配置
+ * measure: { fontSize, fontFamily, src, fontWeight }
+ * truncate: { fontSize, fontFamily, src, fontWeight, width, row, lack, ellipsis }
+ */
+EasyFont.prototype.option = function (option) {
+  this.$option = option;
+  this._settedOption = true;
+  verifyOption.bind(this)();
+  return this;
+}
+
+/**
+ * 设置操作
+ * @param operation EasyFont.operationType
+ */
+EasyFont.prototype.operation = function (operation) {
+  if (!verifyOperation.bind(this, operation)()) {
+    throw 'operation should be one of ' + Object.keys(this.operationType).toString();
+  }
+  this.$operationName = operation;
+
+  verifyOption.bind(this)();
+  // 校验操作类型
+  function verifyOperation (operation) {
+    return Object.keys(this.operationType).some(function (ele) {
+      return operation === ele;
+    })
+  }
+  return this;
+}
+
+/**
+ * 操作类型
+ */
+EasyFont.prototype.operationType = {
+  measure: 'measure',
+  truncate: 'truncate',
+}
+
+/**
+ * 终点函数，获取结果值
+ * @return Promise
+ */
+EasyFont.prototype.value = function () {
+
+  /**
+   * 测量字符串长度信息
+   * @option { fontFamily, fontSize, fontWeight, src }
+   */
+  if (this.$operationName === this.operationType.measure) {
+    return getMeasure.bind(this, this.$string, this.$option)();
+  }
+
+  // 字符串规则截断
+  if (this.$operationName === this.operationType.truncate) {
+    var originString = this.$string;
+    var option = this.$option;
+    return new Promise(function (resolve) {
+      var _truncateOpt = Object.assign({}, {
+        lack: 0,
+        ellipsis: '...'
+      }, option);
+
+      var _resultString = originString + _truncateOpt.ellipsis;
+      getMeasure.bind(this, _resultString, option)().then(function(result) {
+        if (result) {
+          var originRow = result.width / _truncateOpt.width;
+          // 字符串短的情况
+          if (originRow < _truncateOpt.row) {
+            resolve({
+              result: _resultString,
+              origin: originString,
+              row: _truncateOpt.row,
+              boxWidth: _truncateOpt.width,
+              lack: _truncateOpt.lack,
+              ellipsis: _truncateOpt.ellipsis
+            });
+          } else {
+            var lastIndex = Math.floor(_truncateOpt.width * _resultString.length / result.lenght) - 1;
+
+            resolve({
+              result: _resultString.slice(0, lastIndex),
+              origin: originString,
+              row: _truncateOpt.row,
+              boxWidth: _truncateOpt.width,
+              lack: _truncateOpt.lack,
+              ellipsis: _truncateOpt.ellipsis
+            });
+          }
+        }
+      });
+    });
+  }
+}
+
+// 校验配置
+function verifyOption () {
+  if (this.$operationName && this.$operationName === this.operationType.truncate && this._settedOption && (!this.$option.width || !this.$option.row)) {
+    throw new Error(this.$operationName + ' operation option mandotory: width, row');
+  }
+}
+
+// 测量字符串
+function getMeasure(str, option) {
+  return new Promise(function (resolve) {
+    // 判断依赖
     if (!Font) {
-      console.error('easyfont should run into browser env.');
+      console.error('EasyFont Error: easyfont should run into browser env.');
       resolve(null);
     }
-    // config option
+    // 配置信息
     var _opt = Object.assign({
       fontFamily: "'PingFang SC', sans-serif",
       fontSize: 16,
@@ -15,7 +140,7 @@ var strDOMLength = function (str, option) {
       src: '', // use system font(s)
     }, option || {});
 
-    // empty string
+    // 『空』字符串的情况
     if (!str) {
       resolve({
         width: 0,
@@ -25,10 +150,9 @@ var strDOMLength = function (str, option) {
       })
     }
 
-    // let's load a font!
+    // 初始化
     var font = new Font();
-
-    // set up the onload handler
+    // 加载
     font.onload = function () {
       var measure = font.measureText(str, _opt.fontSize, _opt.fontWeight);
       resolve({
@@ -39,16 +163,22 @@ var strDOMLength = function (str, option) {
       });
     }
 
-    // error handler
+    // 加载失败
     font.onerror = function (err) {
-      console.error(err);
-      resolve(null); 
+      console.error('EasyFont Error: ' + err);
+      resolve(null);
     }
 
-    // then kick off font loading by assigning the "src" property
+    // 使用src标记加载事件完成
     font.fontFamily = _opt.fontFamily;
     font.src = _opt.src;
   });
-};
+}
 
-module.exports = strDOMLength;
+module.exports = EasyFont;
+
+
+/*
+输入： string, width, row, lack, ellipsis
+
+*/
